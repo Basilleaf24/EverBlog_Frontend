c38485d2acf199dfe92eac601a1f44deb9b74493
Prathibha R <prathi2kgrs@gmail.com>
2021-10-14 07:30:54 +0530

    Frontend in - Creating React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card, CardTitle } from 'reactstrap';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card body inverse style={{backgroundColor:'#333', borderColor:'#333'}}>
+        <ToastContainer/>
+        
+        <CardTitle>
+          Hello World
+        </CardTitle>
+      </Card>
     </div>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ListGroup, Row } from 'reactstrap'
+
+const Menu = ()=>{
+    return(
+        <ListGroup>
+            <Row>
+                <Col>
+                </Col>
+
+                <Col>
+                </Col>
+            </Row>
+        </ListGroup>
+    )
+}
+export default Menu
