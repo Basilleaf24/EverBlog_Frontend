ae7287bbf56181634359cc784cd77b092833fc43
Prathibha R <prathi2kgrs@gmail.com>
2021-10-15 14:01:39 +0530

    Connecting the dots

--- src/components/AllBlog.js
+++ src/components/AllBlog.js
@@ -1,20 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, CardTitle } from 'reactstrap';
+import axios from 'axios';
 import Blog from './Blog';
+import base_url from './../service/serviceapi';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AllBlogs = ()=>{
 
-    const [blogs, setBlogs] = useState([
-        {blogId:"12345", title:"Test5", content:"Test content5"},
-        {blogId:"12346", title:"Test6", content:"Test content6"},
-        {blogId:"12347", title:"Test7", content:"Test content7"},
-        {blogId:"12348", title:"Test8", content:"Test content8"},
-        {blogId:"12349", title:"Test9", content:"Test content9"}
-    ]);
+    const [blogs, setBlogs] = useState([]);
+
+    //useEffect should call load from db method
+    useEffect(()=>{
+        getAllBlogsFromServer()
+    }, [])
+
+    const getAllBlogsFromServer = ()=>{
+        axios.get(`${base_url}/all`).then(
+            (response)=>{
+                console.log(response);
+                setBlogs(response.data);//if response is present, setBlogs adds the response into blogs array
+                toast.success("All blogs successfully loaded from server!")
+            },
+            (error)=>{
+                toast.error("Something went wrong on server!")
+            }
+        )
+    }
 
     return(
         <Card body inverse color="info">
-            <CardTitle className="display-2">
+            <CardTitle className="display-3">
                 Blogs to read....
             </CardTitle>     
             {
