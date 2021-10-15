f0feb715fa1fe04b876fdbd2b2816115ac9d5a3b
Prathibha R <prathi2kgrs@gmail.com>
2021-10-15 19:52:39 +0530

    Add/ Create Blog Functionality

--- src/components/AddBlog.js
+++ src/components/AddBlog.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 import { Card, CardBody, CardTitle, Input, Container, Button } from 'reactstrap';
+import base_url from './../service/serviceapi';
 
 const AddBlog = ()=>{
+
+    const [blog, setBlog] =  useState({});
+
+    const handleForm = (param)=>{
+        postData(blog);
+        param.preventDefault();
+    }
+
+    const postData = (data)=>{
+        axios.post(`${base_url}/create`, data).then(   //for post and put request, data has to be passed
+            (response)=>{
+                toast.info("New Blog Posted Successfully!");
+            }, (error)=>{
+                toast.error("Something went wrong on server!");
+            }
+        )
+    }
+
     return(
         <Card body inverse color="info">
             <CardBody>
-                <form onSubmit="#">
+                <form onSubmit={handleForm}>
                     <CardTitle className="display-2">
                         New Blog
                     </CardTitle>
                     <Input placeholder="Enter the Blog Title here."
-                    onChange="#"/>
+                    onChange={(e)=>{
+                        setBlog({...blog, title:e.target.value})
+                    }}/>
                     <br/>
                     <Input type="textarea" rows="10"
                     placeholder="Enter the Blog Content here."
-                    onChange="#"/>
+                    onChange={(e)=>{
+                        setBlog({...blog, content:e.target.value})
+                    }}/>
                     <br/>
                     <Container>
                         <Button type="submit" color="success" style={{marginRight:15+"px"}}>
